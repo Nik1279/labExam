f3dc7dc7f92ed2ecd1492cd12305240b1cc13170
sam <sam@gmail.com>
2023-07-25 08:08:24 +0530

    file added successfully

---
+++ ThreeNum.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ThreeNum{
+    public static void main(String [] args){
+        Scanner sc= new Scanner (System.in);
+        System.out.println("enter the three numbers");
+        int a =sc.nextInt();int b=sc.nextInt();int c=sc.nextInt();
+        System.out.println("sum of number is"+(a+b+c));
+
+    }
+}
