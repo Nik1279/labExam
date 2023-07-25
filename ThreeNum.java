53340f762b3515f10d2d59a4b417bbc31f2f5b80
sam <sam@gmail.com>
2023-07-25 08:34:58 +0530

    file updated successfully

--- ThreeNum.java
+++ ThreeNum.java
@@ -4,7 +4,11 @@ public class ThreeNum{
         Scanner sc= new Scanner (System.in);
         System.out.println("enter the three numbers");
         int a =sc.nextInt();int b=sc.nextInt();int c=sc.nextInt();
-        System.out.println("sum of number is"+(a+b+c)/3);
+        System.out.println("average of number is"+(a+b+c)/3);
+        if(a>b && b>c) System.out.println("largest is "+a);
+        else if(b>a && b>c)System.out.println("largest is"+b);
+        else
+        System.out.println("largest is " + c);
 
     }
 }
